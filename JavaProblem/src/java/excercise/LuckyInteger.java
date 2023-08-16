ad9406700df3bc6c1ddba10e4201f8a3a44fba5b
sugny <31289384+sugny@users.noreply.github.com>
2023-08-16 10:48:58 -0500

    Add files via upload

---
+++ JavaProblem/src/java/excercise/LuckyInteger.java
@@ -0,0 +1,21 @@
+package java.excercise;
+
+public class LuckyInteger {
+
+	public static void main(String[] args) {
+		
+		 int input[] = {2,2,3,4};
+		 int length = input.length;
+		 
+		 for(int i=0;i<length;i++){
+			 
+			 if(input[i]==input[i+1]){
+				 System.out.println(input[i]);
+				 
+		 }
+		
+		 }
+		
+	}
+
+}
